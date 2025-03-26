d8d391f2098b63ea4af9e231cdf76134b2c52679
IshankSharma2178 <ishank.sharma.9891@gmail.com>
2025-03-26 16:53:18 +0530

    fix some bugs

--- src/features/auth/components/Sign-Up-Card.tsx
+++ src/features/auth/components/Sign-Up-Card.tsx
@@ -2,13 +2,7 @@
 
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import {
